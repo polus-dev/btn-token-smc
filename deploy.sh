df242c491e7005c8302861d1ee85112bb276780f
cryshado <mirmiclub@gmail.com>
2022-06-18 02:19:12 +0300

    -

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-
-fift -s ./fift/minter-deploy.fif \
-    10000000    \   # max-supply 10 000 000
-    4000000     \   # max ico    4  000 000
-    0.0159      \   # price
-    1970867460  \   # ico end-time (~10 years)
-    EQD78TEut58n_42WAUc2L2bn6UU5_FkZhGOQ6MQhJtmpXvYY \
-    ipfs://bafkreidmxzvlum7mz5cxw45kpwuxw5hczdggwgafbkawamggplhy3hahpe
